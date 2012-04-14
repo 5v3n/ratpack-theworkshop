d1769f6385c7543d1e5b4d3ffc6e45ec557a22c4
Sven Kraeuter | 5v3n <sven.kraeuter@gmail.com>
2012-04-14 22:46:08 +0200

    rat pack client. abusing occupy ping pong for testing. next: rat pack server

--- arduino/ratpack_workshop/ratpack_workshop.ino
+++ arduino/ratpack_workshop/ratpack_workshop.ino
@@ -1,56 +1,118 @@
+#include "WiFly.h"
+#include "Config.h"
+
 /*
-  Button
- 
- Turns on and off a light emitting diode(LED) connected to digital  
- pin 13, when pressing a pushbutton attached to pin 2. 
- 
- 
- The circuit:
- * LED attached from pin 13 to ground 
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
- 
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
- 
- 
- created 2005
- by DojoDave <http://www.0j0.org>
- modified 30 Aug 2011
- by Tom Igoe
- 
- This example code is in the public domain.
- 
- http://www.arduino.cc/en/Tutorial/Button
- */
+* rat pack workshop result
+*
+* basically this is occupy ping pong with a push button replacing the infrared motion sensor,
+* see https://github.com/makersandco/Occupy-Ping-Pong for details.
+*
+* sven kraeuter for makers & co.
+*
+* http://makersand.co
+*
+* wifly library for arduino 1.0: https://github.com/timr/WiFly-Shield
+*
+*/
 
-// constants won't change. They're used here to 
-// set pin numbers:
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+byte motionCounter = 0;
 
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+Client client(HOST, 80);
 
-void setup() {
-  // initialize the LED pin as an output:
+void setup(){
+  //------------------------ led setup ------------------------
   pinMode(ledPin, OUTPUT);      
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);     
+  //------------------------ button setup ------------------------
+  pinMode(buttonPin, INPUT);    
+  //------------------------ wifly setup ----------------------------------
+  Serial.begin(9600);
+  
+  WiFly.begin();
+  
+  if (WiFly.join(ssid, passphrase)) {
+    Serial.println("associted with wifly network.");
+  } 
+  else {
+    Serial.println("Association failed.");
+    while (1) {
+      // Hang on failure.
+    }
+  }  
 }
 
 void loop(){
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {     
-    // turn LED on:    
-    digitalWrite(ledPin, HIGH);  
-  } 
-  else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW); 
+  //------------------- button part ---------------------------------------
+  if(digitalRead(buttonPin) == LOW){
+    Serial.println("Button NOT pressed.");
+    //--------- send status to server
+    connectClient(generateHttpPut(HOST, RESOURCE, '0'));
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
   }
+  else if(digitalRead(buttonPin) == HIGH){
+    Serial.println("Button IS pressed!");
+    //--------- send status to server 
+    connectClient(generateHttpPut(HOST, RESOURCE, '1'));
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+  }
+  
+  //-------------------- signalling led part -------------------------------------
+  char postString[255];
+  sprintf(postString, "GET http://%s%s HTTP/1.0\r\n", HOST, RESOURCE);
+  connectClient(postString);
+  
+  if (client.available()) {
+    // receiving status from server
+    if(client.find("\"occupied\": ")){
+      char state[1];
+      client.readBytes(state, 1);
+      if(atoi(state) == 1){
+        Serial.println("PING PONG OCCUPIED. Erm I mean, button pressed.");
+        digitalWrite(ledPin, HIGH);
+      }
+      else if(atoi(state) == 0) {
+        Serial.println("PING PONG VACANT.  Erm I mean, button not pressed.");
+        digitalWrite(ledPin, LOW);
+      }
+    } 
+    if (!client.connected()) {
+      Serial.println();
+      Serial.println("disconnecting.");
+      client.stop();
+    }
+    
+  }
+  
+  
+  delay(pause); 
+}
+
+char* generateHttpPut(char* host, char* resource, char occupied){
+  char postString[255];
+  sprintf(postString, "PUT %s HTTP/1.1\r\nUser-Agent: %s\r\nHost: %s\r\nContent-Length: 15\r\nContent-Type: application/json\r\n\r\n{\"occupied\": %c}"
+  , resource, USERAGENT, host, occupied);
+
+  return postString;
+}
+
+void connectClient(char* http_request){
+
+  
+  if (client.connect()) {
+    Serial.println("connected");
+    Serial.println(http_request);
+    client.print(http_request);
+    client.println();
+  } else {
+    Serial.println("connection failed");
+  }
+  
+  Serial.print("Waiting for the response...");
+  while(!client.available()){
+    Serial.print('.');
+  }
+  Serial.println();
 }
